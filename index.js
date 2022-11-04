fb1da2c302ab4ee88c0dc95e9dbad0e478421830
Emmanuel Olowoyo <ayleosoftsms@gmail.com>
2022-11-04 21:11:54 +0100

    install body-parser and loader

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ const express = require("express");
 const { json } = require("express");
 const flights = require("./controllers/flightController");
 const models = require("./models/Flight");
+const { post } = require("./routes/flightRoute");
 const routes = require("./routes/flightRoute");
+const bodyParser = require("body-parser");
+
+
+
+
+
+
 
 const app = express();
 
 app.use(json());
 
 app.use("/", routes);
+
+
+// Patch code in node js?
+routes.patch('/Postid', async(req, res) => {
+  try{
+    const updatePost = await post.updatePost({ _id: req.params.postid},);
+    res.json(updatedPost);
+  }catch(err){
+    res.json({message:err});
+  }
+  })
+
+//delete code
+routes.delete('/Postid', async(req, res) =>{
+  try{
+    const removedPost = await post.remove({ _id: req.params.postid});
+    res.json(removePost);
+  }catch(err){res.json({message: err});}
+})
+
+
+
+
 
 const port = process.env.PORT || 3000;
 
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -5,19 +5,6 @@ const controller = require('../controllers/flightController');
 
 router.get('/', controller.example)
 
-/*
-? Delete a flight
-*/
-router.delete('/:flightId', async (req, res) => {
-    try {
-        const removedflight = await flight.remove({_Id:req.params.flightId});
-        res.json(removedflight);
-    } catch (err) {
-        res.json({message:err});
-    });
-
-
-
 
 
 module.exports = router;
